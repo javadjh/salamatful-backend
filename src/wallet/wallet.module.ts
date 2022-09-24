92c9bc231b4513ecb96b817d81887a934b91a5e8
SohaKhafab <soha.gholami1384@gmail.com>
2022-09-24 16:16:28 +0330

    add folan

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VideoSchema } from 'src/video/video.schema';
+import { WalletSchema } from './wallet.schema';
+import { User, UserSchema } from 'src/user/user.schema';
 
 @Module({
   controllers: [WalletController],
-  providers: [WalletService]
+  providers: [WalletService],
+  imports: [MongooseModule.forFeature([
+    { name: "User", schema: UserSchema },
+    {name:"Wallet", schema:WalletSchema},
+  ])],  
 })
 export class WalletModule {}
