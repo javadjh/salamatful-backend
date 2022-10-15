4a7078b8ca4a58df6309206d05e67517534c0cbb
Gustav <gustavbadie@gmail.com>
2022-10-15 21:30:51 +0330

    sort audios and videos in course

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,44 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { AccessDocument } from 'src/access/access.schema';
-import { AudioDocument } from 'src/audio/audio.schema';
-import { BlogDocument } from 'src/blog/blog.schema';
-import config from 'src/config';
-import { GalleryDocument } from 'src/gallery/gallery.schema';
-import { PurchaseDocument } from 'src/purchase/purchase.schema';
-import { UserDocument } from 'src/user/user.schema';
-import { VideoDocument } from 'src/video/video.schema';
-import { CourseDocument } from './course.schema';
-import {DynamicsDocument} from "../dynamics/dynamics.schema";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { AccessDocument } from "src/access/access.schema";
+import { AudioDocument } from "src/audio/audio.schema";
+import { BlogDocument } from "src/blog/blog.schema";
+import config from "src/config";
+import { GalleryDocument } from "src/gallery/gallery.schema";
+import { PurchaseDocument } from "src/purchase/purchase.schema";
+import { UserDocument } from "src/user/user.schema";
+import { VideoDocument } from "src/video/video.schema";
+import { CourseDocument } from "./course.schema";
+import { DynamicsDocument } from "../dynamics/dynamics.schema";
 
 @Injectable()
 export class CourseService {
   constructor(
-    @InjectModel('Course') private courseModel: Model<CourseDocument>,
-    @InjectModel('User') private userModel: Model<UserDocument>,
-    @InjectModel('Video') private videoModel: Model<VideoDocument>,
-    @InjectModel('Blog') private blogModel: Model<BlogDocument>,
-    @InjectModel('Audio') private audioModel: Model<AudioDocument>,
-    @InjectModel('Gallery') private galleryModel: Model<GalleryDocument>,
-    @InjectModel('Purchase') private purchaseModel: Model<PurchaseDocument>,
-    @InjectModel('Dynamics') private dynamicsModel: Model<DynamicsDocument>,
-  ) { }
+    @InjectModel("Course") private courseModel: Model<CourseDocument>,
+    @InjectModel("User") private userModel: Model<UserDocument>,
+    @InjectModel("Video") private videoModel: Model<VideoDocument>,
+    @InjectModel("Blog") private blogModel: Model<BlogDocument>,
+    @InjectModel("Audio") private audioModel: Model<AudioDocument>,
+    @InjectModel("Gallery") private galleryModel: Model<GalleryDocument>,
+    @InjectModel("Purchase") private purchaseModel: Model<PurchaseDocument>,
+    @InjectModel("Dynamics") private dynamicsModel: Model<DynamicsDocument>
+  ) {
+  }
 
   async findBySlug(params, userId): Promise<any> {
     try {
       const { slug } = params;
       let videoCategories = [];
       const course = await this.courseModel.findOne({ slug });
-      const user = userId ? await this.userModel.findById(userId, 'sub') : null;
+      const user = userId ? await this.userModel.findById(userId, "sub") : null;
       let galleries = [];
       const subscribed =
         user && user.sub && user.sub.getTime() >= new Date().getTime();
       let audios, videos, blogs;
+      let audiosInOrder = [];
       if (course) {
         if (course.hasAccess) {
           const hasAccess = await this.purchaseModel.findOne({ userId: userId, courseId: course.id });
-          if (!hasAccess) return { code: -1, message: 'Access denied' }
+          if (!hasAccess) return { code: -1, message: "Access denied" };
         }
         if (course.bg) {
           course.bg.path = `${config.serverURL}${config.courseImages}/${course.bg.path}`;
@@ -54,38 +56,45 @@ export class CourseService {
             gallery.lock = false;
           }
         }
-        audios = await this.audioModel.find({ courseId: course.id });
-        for (const audio of audios) {
-          if (audio.file) {
-            audio.file.path = `${config.serverURL}${config.defaultImages}/${audio.file.path}`;
-          }
-          if (audio.lock && subscribed) {
-            audio.lock = false;
+        audios = course.audios;
+        for (let audio of audios) {
+          audio = await this.audioModel.findById(audio);
+          if (audio) {
+            if (audio.file) {
+              audio.file.path = `${config.serverURL}${config.defaultImages}/${audio.file.path}`;
+            }
+            if (audio.lock && subscribed) {
+              audio.lock = false;
+            }
+            audiosInOrder.push(audio);
           }
         }
-        videos = await this.videoModel.find({ courseId: course.id });
-        for (const video of videos) {
-          if (video.file) {
-            video.file.path = `${config.serverURL}${config.defaultImages}/${video.file.path}`;
-          }
-          if (video.poster) {
-            video.poster.path = `${config.serverURL}${config.defaultImages}/${video.poster.path}`;
-          }
-          if (video.lock && subscribed) {
-            video.lock = false;
-          }
-          if (video.category) {
-            const index = videoCategories.map(cat => cat.categoryName).indexOf(video.category);
-            if (index >= 0) {
-              videoCategories[index].videos.push(video);
-            } else {
-              videoCategories = [...videoCategories, { categoryName: video.category, videos: [video] }];
+        videos = course.videos;
+        for (let video of videos) {
+          video = await this.videoModel.findById(video);
+          if (video) {
+            if (video.file) {
+              video.file.path = `${config.serverURL}${config.defaultImages}/${video.file.path}`;
+            }
+            if (video.poster) {
+              video.poster.path = `${config.serverURL}${config.defaultImages}/${video.poster.path}`;
+            }
+            if (video.lock && subscribed) {
+              video.lock = false;
+            }
+            if (video.category) {
+              const index = videoCategories.map(cat => cat.categoryName).indexOf(video.category);
+              if (index >= 0) {
+                videoCategories[index].videos.push(video);
+              } else {
+                videoCategories = [...videoCategories, { categoryName: video.category, videos: [video] }];
+              }
             }
           }
         }
         blogs = await this.blogModel.find(
           { courseId: course.id },
-          '_id title category lock img slug',
+          "_id title category lock img slug"
         );
         blogs.map((blog) => {
           if (blog.img) {
@@ -96,9 +105,9 @@ export class CourseService {
           }
         });
       }
-      return { course, audios, videos: videoCategories, blogs, galleries };
+      return { course, audios: audiosInOrder, videos: videoCategories, blogs, galleries };
     } catch (error) {
-      return { code: -1, message: 'Error occurred' };
+      return { code: -1, message: "Error occurred" };
     }
   }
 
@@ -106,23 +115,23 @@ export class CourseService {
     try {
       const { courseId } = body;
       const user = await this.userModel.findById(userId);
-      const course = await this.courseModel.findById(courseId, '_id name bg');
+      const course = await this.courseModel.findById(courseId, "_id name bg");
       if (user && course) {
         const hasLikedBefore = await this.userModel.findOne({
-          $and: [{ _id: userId }, { fav: { $elemMatch: { cId: courseId } } }],
+          $and: [{ _id: userId }, { fav: { $elemMatch: { cId: courseId } } }]
         });
         if (hasLikedBefore) {
           // delete the like record from the user model
           await this.userModel.updateOne(
             { _id: userId },
-            { $pull: { fav: { cId: courseId } } },
+            { $pull: { fav: { cId: courseId } } }
           );
           // delete the like record from the course model
           await this.courseModel.updateOne(
             { _id: courseId },
-            { $inc: { likes: -1 } },
+            { $inc: { likes: -1 } }
           );
-          return { code: 1, message: 'Action performed', data: false };
+          return { code: 1, message: "Action performed", data: false };
         }
         // like the course in user model
         await this.userModel.updateOne(
@@ -134,21 +143,21 @@ export class CourseService {
                 name: course.name,
                 bg:
                   course.bg &&
-                  `${config.serverURL}${config.defaultImages}/${course.bg.path}`,
-              },
-            },
-          },
+                  `${config.serverURL}${config.defaultImages}/${course.bg.path}`
+              }
+            }
+          }
         );
         // like the course in course model
         await this.courseModel.updateOne(
           { _id: courseId },
-          { $inc: { likes: 1 } },
+          { $inc: { likes: 1 } }
         );
-        return { code: 1, message: 'Action performed', data: true };
+        return { code: 1, message: "Action performed", data: true };
       }
-      return { code: 0, message: 'No match found' };
+      return { code: 0, message: "No match found" };
     } catch (error) {
-      return { code: -1, message: 'An Error Occurrred' };
+      return { code: -1, message: "An Error Occurrred" };
     }
   }
 }
