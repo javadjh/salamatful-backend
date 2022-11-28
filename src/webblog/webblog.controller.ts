0a60c9d82395d21bf09acd759e4d9eda17aabd47
shayrad <shuyrud@gmail.com>
2022-11-28 18:36:21 +0330

    shayrad

--- src/webblog/webblog.controller.ts
+++ src/webblog/webblog.controller.ts
@@ -1,7 +1,48 @@
-import { Controller } from '@nestjs/common';
-import { WebblogService } from './webblog.service';
+import { Controller, Get, Param, Query, Res } from "@nestjs/common";
+import { Response } from "express";
+import { BlogService } from "./webblog.service";
 
 @Controller('webblog')
 export class WebblogController {
-  constructor(private readonly webblogService: WebblogService) {}
+  constructor(private readonly webblogService: BlogService) {}
+}
+@Controller("blog")
+export class webBlogController {
+  constructor(private readonly blogService: BlogService) {
+  }
+
+  @Get("/blog/:slug")
+  async getBlog(@Param() params, @Res() res: Response): Promise<any> {
+    return await this.blogService.findBySlug(params,));
+  }
+
+  @Get("/category")
+  async getCategories(): Promise<any> {
+    return await this.blogService.getCategories();
+  }
+
+  @Get("/category/:category")
+  async getCategoryBlogs(@Param() params): Promise<any> {
+   const tag= await this.blogService.getBlogs(params);
+  }
+
+  @Get()
+  async getAllBlogs(): Promise<any> {
+    return await this.blogService.getEntireBlogs();
+  }
+
+  @Get("/suggestions")
+  async getSuggestions(@Query() query) {
+    return await this.blogService.getSuggestedBlogs(query.limit, query.category);
+  }
+
+  @Get("/recent")
+  async getRecent(@Query() query) {
+    return await this.blogService.getRecentBlogs(query.limit);
+  }
+
+  @Get("/all-blogs")
+  async getAllBlogsWithCategory(@Res() res: Response) {
+     return await this.blogService.getAllwebBlogsWithCategory(res.locals.userId);
+  }
 }
--- src/webblog/webblog.service.ts
+++ src/webblog/webblog.service.ts
@@ -1,4 +1,100 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import config from "src/config";
+import { UserDocument } from "src/user/user.schema";
+import { WebBlogDocument } from "./webBlog.schema";
+import { PurchaseDocument } from "../purchase/purchase.schema";
 
 @Injectable()
-export class WebblogService {}
+export class BlogService {
+    constructor(
+        @InjectModel("WebBlog") private webblogModel: Model<WebBlogDocument>,
+
+    ) {
+    }
+
+    async findBySlug(params, userId): Promise<any> {
+        const { slug } = params;
+        const user = userId ? await this.userModel.findById(userId, "sub") : null;
+        const subscribed =
+            user && user.sub && user.sub.getTime() >= new Date().getTime();
+        const blog = await this.webblogModel.findOne({ slug });
+        if (blog) {
+            if (blog.img) {
+                blog.img.path = `${config.serverURL}${config.blogImages}/${blog.img.path}`;
+            }
+            if (blog.lock && subscribed) {
+                blog.lock = false;
+            }
+            return blog;
+        }
+        throw new NotFoundException("Invalid Slug");
+    }
+
+    async getCategories(): Promise<any> {
+        try {
+            return await this.webblogModel
+                .find({}, 'tags')
+        } catch (error) {
+            return { code: -1, message: "Error" };
+        }
+    }
+
+    async getBlogs(params): Promise<any> {
+        try {
+            const blogs = await this.webblogModel.find(
+                { cats: params.category },
+                "_id slug title author pDate img lock"
+            );
+            if (blogs) {
+                for (const blog of blogs) {
+                    if (blog.img) {
+                        blog.img.path = `${config.serverURL}${config.blogImages}/${blog.img.path}`;
+                    }
+                }
+            }
+            return blogs;
+        } catch (error) {
+            return { code: -1, message: "Error" };
+        }
+    }
+
+    async getEntireBlogs(): Promise<any> {
+        try {
+            return this.fixImagePaths(await this.webblogModel.find({}, 'title slug meta pDate'));
+        } catch (error) {
+            return { code: -1, message: "An error occurred" };
+        }
+    }
+
+    async getSuggestedBlogs(limit: number, category: string): Promise<any> {
+        try {
+            return this.fixImagePaths(await this.webblogModel.find({
+                cats: category,
+                lock: false
+            }).sort({ _id: 1 }).limit(limit));
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
+
+    async getRecentBlogs(limit: number): Promise<any> {
+        try {
+            return this.fixImagePaths(await this.webblogModel.find({}).sort({ _id: -1 }).limit(limit));
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
+
+    fixImagePaths(blogs: Array<any>): any {
+        for (const blog of blogs) {
+            if (blog.img) {
+                blog.img.path = `${config.serverURL}${config.blogImages}/${blog.img.path}`;
+            }
+        }
+        return blogs;
+    }
+}
