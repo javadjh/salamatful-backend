f261d423e2a22d0a2f4b2f75483b69f2de1140e6
gustav <gustavbadie@gmail.com>
2023-02-04 12:32:37 +0330

    test sms api

--- src/lib/kavenegar.ts
+++ src/lib/kavenegar.ts
@@ -50,6 +50,8 @@ class Kavenegar {
   _generateEndpoint(method) {
     let apiBase = getEnv('KAVENEGAR_API_BASE');
     let apiKey = getEnv('KAVENEGAR_API_KEY');
+    console.log('url:');
+    console.log(`${apiBase}/${apiKey}/${method}`);
     return `${apiBase}/${apiKey}/${method}`;
   }
 }
